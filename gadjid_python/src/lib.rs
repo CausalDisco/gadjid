2e9318775e65efed553c580b19ec517fbcd4496b
Theo <12211068+Whatisthisname@users.noreply.github.com>
2024-03-06 15:41:55 +0100

    force users to specify edge semantics

--- gadjid_python/src/lib.rs
+++ gadjid_python/src/lib.rs
@@ -15,6 +15,9 @@ use ::gadjid::graph_operations::shd as rust_shd;
 use ::gadjid::graph_operations::sid as rust_sid;
 use ::gadjid::EdgelistIterator;
 use ::gadjid::PDAG;
+use anyhow::bail;
+use pyo3::exceptions::PyTypeError;
+use pyo3::prelude::*;
 
 use numpy_ndarray_handler::try_from as try_from_dense;
 use scipy_sparse_handler::try_from as try_from_sparse;
@@ -64,47 +67,78 @@ fn gadjid(_py: Python, m: &PyModule) -> PyResult<()> {
     Ok(())
 }
 
+const ROW_TO_COL: &str = "row->col";
+const COL_TO_ROW: &str = "col->row";
+
+fn edge_direction_semantics_is_row_to_col(edge_direction: &str) -> PyResult<bool> {
+    match edge_direction {
+        ROW_TO_COL => Ok(true),
+        COL_TO_ROW => Ok(false),
+        _ => Err(PyErr::new::<PyTypeError, _>(format!(
+            "edge_direction argument must be either a string containing (exactly) '{}' or '{}'",
+            ROW_TO_COL, COL_TO_ROW
+        ))),
+    }
+}
+
 /// Ancestor Adjustment Identification Distance between two DAG / CPDAG adjacency matrices (sparse or dense)
 #[pyfunction]
-pub fn ancestor_aid(g_true: &PyAny, g_guess: &PyAny) -> PyResult<(f64, usize)> {
-    let graph_truth = graph_from_pyobject(g_true)?;
-    let graph_guess = graph_from_pyobject(g_guess)?;
+pub fn ancestor_aid(
+    g_true: &PyAny,
+    g_guess: &PyAny,
+    edge_semantics: &str,
+) -> PyResult<(f64, usize)> {
+    let row_to_col = edge_direction_semantics_is_row_to_col(edge_semantics)?;
+    let graph_truth = graph_from_pyobject(g_true, row_to_col)?;
+    let graph_guess = graph_from_pyobject(g_guess, row_to_col)?;
     let (normalized_distance, n_errors) = rust_ancestor_aid(&graph_truth, &graph_guess);
     Ok((normalized_distance, n_errors))
 }
 
 /// Optimal Adjustment Identification Distance between two DAG / CPDAG adjacency matrices (sparse or dense)
 #[pyfunction]
-pub fn oset_aid(g_true: &PyAny, g_guess: &PyAny) -> PyResult<(f64, usize)> {
-    let graph_truth = graph_from_pyobject(g_true)?;
-    let graph_guess = graph_from_pyobject(g_guess)?;
+pub fn oset_aid(g_true: &PyAny, g_guess: &PyAny, edge_semantics: &str) -> PyResult<(f64, usize)> {
+    let row_to_col = edge_direction_semantics_is_row_to_col(edge_semantics)?;
+    let graph_truth = graph_from_pyobject(g_true, row_to_col)?;
+    let graph_guess = graph_from_pyobject(g_guess, row_to_col)?;
     let (normalized_distance, n_errors) = rust_oset_aid(&graph_truth, &graph_guess);
     Ok((normalized_distance, n_errors))
 }
 
 /// Parent Adjustment Identification Distance between two DAG / CPDAG adjacency matrices (sparse or dense)
 #[pyfunction]
-pub fn parent_aid(g_true: &PyAny, g_guess: &PyAny) -> PyResult<(f64, usize)> {
-    let graph_truth = graph_from_pyobject(g_true)?;
-    let graph_guess = graph_from_pyobject(g_guess)?;
+pub fn parent_aid(
+    g_true: &PyAny,
+    g_guess: &PyAny,
+    edge_semantics: &str,
+) -> PyResult<(f64, usize)> {
+    let row_to_col = edge_direction_semantics_is_row_to_col(edge_semantics)?;
+    let graph_truth = graph_from_pyobject(g_true, row_to_col)?;
+    let graph_guess = graph_from_pyobject(g_guess, row_to_col)?;
     let (normalized_distance, n_errors) = rust_parent_aid(&graph_truth, &graph_guess);
     Ok((normalized_distance, n_errors))
 }
 
 /// Structural Hamming Distance between two DAG / CPDAG adjacency matrices (sparse or dense)
 #[pyfunction]
-pub fn shd(g_true: &PyAny, g_guess: &PyAny) -> PyResult<(f64, usize)> {
-    let graph_truth = graph_from_pyobject(g_true)?;
-    let graph_guess = graph_from_pyobject(g_guess)?;
+pub fn shd(g_true: &PyAny, g_guess: &PyAny, edge_semantics: &str) -> PyResult<(f64, usize)> {
+    let row_to_col = edge_direction_semantics_is_row_to_col(edge_semantics)?;
+    let graph_truth = graph_from_pyobject(g_true, row_to_col)?;
+    let graph_guess = graph_from_pyobject(g_guess, row_to_col)?;
     let (normalized_distance, n_errors) = rust_shd(&graph_truth, &graph_guess);
     Ok((normalized_distance, n_errors))
 }
 
 /// Structural Identification Distance between two DAG adjacency matrices (sparse or dense)
 #[pyfunction]
-pub fn sid(g_true: &PyAny, g_guess: &PyAny) -> anyhow::Result<(f64, usize)> {
-    let dag_truth = graph_from_pyobject(g_true)?;
-    let dag_guess = graph_from_pyobject(g_guess)?;
+pub fn sid(
+    g_true: &PyAny,
+    g_guess: &PyAny,
+    edge_semantics: &str,
+) -> anyhow::Result<(f64, usize)> {
+    let row_to_col = edge_direction_semantics_is_row_to_col(edge_semantics)?;
+    let dag_truth = graph_from_pyobject(g_true, row_to_col)?;
+    let dag_guess = graph_from_pyobject(g_guess, row_to_col)?;
     let (normalized_distance, n_errors) = rust_sid(&dag_truth, &dag_guess)?;
     Ok((normalized_distance, n_errors))
 }
@@ -112,11 +146,11 @@ pub fn sid(g_true: &PyAny, g_guess: &PyAny) -> anyhow::Result<(f64, usize)> {
 /// Load a graph from a 2D numpy or scipy sparse matrix.
 /// Will load a matrix into a PDAG, automatically loading into a DAG and checking
 /// acyclicity. If undirected edges present, assumes that it encodes as valid CPDAG
-fn graph_from_pyobject(ob: &PyAny) -> anyhow::Result<PDAG> {
+fn graph_from_pyobject(ob: &PyAny, is_row_to_col: bool) -> anyhow::Result<PDAG> {
     // first try to load as np dense matrix
-    match try_from_dense(ob) {
+    match try_from_dense(ob, is_row_to_col) {
         Ok(load_result) => Ok(load_result),
-        Err(e1) => match try_from_sparse(ob) {
+        Err(e1) => match try_from_sparse(ob, is_row_to_col) {
             Ok(graph) => Ok(graph),
             Err(e2) => {
                 let msg = format!(
@@ -135,10 +169,10 @@ as np ndarray or scipy sparse matrix.
 /// acyclicity. If undirected edges present, assumes that it encodes as valid CPDAG
 pub(crate) fn graph_from_iterator(
     iterator: impl Iterator<Item = (usize, usize, i8)>,
-    row_major: bool,
+    row_to_col: bool,
     graph_size: usize,
 ) -> anyhow::Result<PDAG> {
-    match row_major {
+    match row_to_col {
         true => match PDAG::try_from_row_major(EdgelistIterator::into_row_major_edgelist(
             iterator, graph_size,
         )) {
@@ -147,6 +181,7 @@ pub(crate) fn graph_from_iterator(
                 ::gadjid::LoadError::NotAcyclic => bail!(err),
             },
         },
+        // we have a col-to-row matrix
         false => match PDAG::try_from_col_major(EdgelistIterator::into_column_major_edgelist(
             iterator, graph_size,
         )) {
