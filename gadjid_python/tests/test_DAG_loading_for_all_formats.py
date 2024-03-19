52f5d0e781f6abb7cc3c1fbe17deefb8094db7c3
Theo <12211068+Whatisthisname@users.noreply.github.com>
2024-03-19 15:20:11 +0100

    add test for edge_direction argument, update example

--- gadjid_python/tests/test_DAG_loading_for_all_formats.py
+++ gadjid_python/tests/test_DAG_loading_for_all_formats.py
@@ -7,11 +7,47 @@ import gadjid
 
 def make_dag(size, density) -> np.ndarray:
     dense: np.ndarray = np.random.binomial(
-        1, density, size=(size, size)).astype(np.int8)
+        1, density, size=(size, size)
+    ).astype(np.int8)
     # fill lower triangle+diagonal with zeros
     dense = np.triu(dense, 1)
     perm = np.random.permutation(size)
     return dense[perm, :][:, perm]
+
+
+ROW_TO_COL = "from row to col"
+COL_TO_ROW = "from col to row"
+
+
+def test_edge_direction_argument():
+    exps = 10
+    for _exp in range(exps):
+        size = 10
+
+        # make 2 random dags:
+        truth_dag = make_dag(size, 0.5)
+        guess_dag = make_dag(size, 0.5)
+
+        # for all functions that take an edge_direction argument, check that
+        # the result is the same for both edge directions
+        assert gadjid.sid(
+            truth_dag, guess_dag, edge_direction=ROW_TO_COL
+        ) == gadjid.sid(truth_dag.T, guess_dag.T, edge_direction=COL_TO_ROW)
+        assert gadjid.parent_aid(
+            truth_dag, guess_dag, edge_direction=ROW_TO_COL
+        ) == gadjid.parent_aid(
+            truth_dag.T, guess_dag.T, edge_direction=COL_TO_ROW
+        )
+        assert gadjid.ancestor_aid(
+            truth_dag, guess_dag, edge_direction=ROW_TO_COL
+        ) == gadjid.ancestor_aid(
+            truth_dag.T, guess_dag.T, edge_direction=COL_TO_ROW
+        )
+        assert gadjid.oset_aid(
+            truth_dag, guess_dag, edge_direction=ROW_TO_COL
+        ) == gadjid.oset_aid(
+            truth_dag.T, guess_dag.T, edge_direction=COL_TO_ROW
+        )
 
 
 def test_DAG_loading_for_all_formats():
@@ -45,9 +81,14 @@ def test_DAG_loading_for_all_formats():
 
         last_result = None
         for i, matrix in enumerate(matrices):
-            current_result = gadjid.shd(
-                matrix, matrices[0])
+            current_result = gadjid.shd(matrix, matrices[0])
             assert (
                 last_result is None or last_result == current_result
             ), f"failed for {names[i]}"
             last_result = current_result
+
+
+if __name__ == "__main__":
+    test_edge_direction_argument()
+    test_DAG_loading_for_all_formats()
+    print("all tests passed")
