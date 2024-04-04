54edf0394266a1c7d56f1bde71eca1b056b97d90
Sebastian Weichwald <sweichwald@users.noreply.github.com>
2024-04-04 16:12:20 +0200

    Drop speedtests.
    Signed-off-by: Sebastian Weichwald <sweichwald@users.noreply.github.com>

--- gadjid/src/sets.rs
+++ gadjid/src/sets.rs
@@ -13,7 +13,7 @@ pub type NodeSet = FibSet<Node>;
 pub struct FibonacciU64Hasher {
     hash: u64,
     // default is false
-    has_been_applied: bool,
+    has_been_written_to: bool,
 }
 
 // floor(2^64 / GoldenRatio)
@@ -25,11 +25,11 @@ impl Hasher for FibonacciU64Hasher {
     }
 
     fn write_u64(&mut self, n: u64) {
-        if self.has_been_applied {
+        if self.has_been_written_to {
             panic!("FibonacciU64Hasher accepts only exactly one call to write_{{u64, usize}}.")
         }
         self.hash = C.wrapping_mul(n);
-        self.has_been_applied = true;
+        self.has_been_written_to = true;
     }
 
     fn write_usize(&mut self, n: usize) {
@@ -38,48 +38,5 @@ impl Hasher for FibonacciU64Hasher {
 
     fn finish(&self) -> u64 {
         self.hash
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use rand::SeedableRng;
-
-    use super::*;
-
-    #[test]
-    fn speedtest_hash() {
-        let reps = 100;
-        let mut total_time_nanos = 0;
-
-        for _ in 0..reps {
-            let start = std::time::Instant::now();
-            let mut set = NodeSet::default();
-            for i in 0..1000 {
-                set.insert(i);
-            }
-            let elapsed = start.elapsed().as_nanos();
-            total_time_nanos += elapsed;
-        }
-        println!("Elapsed: {} ns", total_time_nanos);
-    }
-
-    #[test]
-    fn speedtest_algos() {
-        let reps = 100;
-        let mut total_time_nanos = 0;
-        let mut rng = &mut rand_chacha::ChaCha8Rng::seed_from_u64(0);
-
-        let truth = crate::PDAG::random_pdag(0.3, 100, &mut rng);
-
-        for _ in 0..reps {
-            let guess = crate::PDAG::random_pdag(0.3, 100, &mut rng);
-
-            let start = std::time::Instant::now();
-            let _ = crate::graph_operations::oset_aid(&truth, &guess);
-            let elapsed = start.elapsed().as_nanos();
-            total_time_nanos += elapsed;
-        }
-        println!("Elapsed: {} ns", total_time_nanos);
     }
 }
